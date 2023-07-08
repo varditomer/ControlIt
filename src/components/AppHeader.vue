35209112a10f154617cdc66c7623e81769aef93f
varditomer <tomer.vardi@gmail.com>
2023-07-08 11:48:57 +0300

    support mdi icons & theme change

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,20 @@
 <template>
-  <v-app-bar color="info" scroll-behavior="elevate">
+  <v-app-bar scroll-behavior="elevate">
     <v-app-bar-title @click="$router.push('/')" class="app-name">
       ControlIt
     </v-app-bar-title>
+    <v-btn icon @click="toggleTheme">
+      <v-icon>mdi-palette</v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
 
 
 <script setup lang="ts">
+import { useTheme } from 'vuetify'
+const theme = useTheme()
+const toggleTheme = () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+
 </script>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ducimus facere hic ipsa nulla eos tempore quas sequi neque odio nesciunt quibusdam consequatur nostrum, consequuntur assumenda. Eum quas fuga ad.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ducimus facere hic ipsa nulla eos tempore quas sequi neque odio nesciunt quibusdam consequatur nostrum, consequuntur assumenda. Eum quas fuga ad.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ducimus facere hic ipsa nulla eos tempore quas sequi neque odio nesciunt quibusdam consequatur nostrum, consequuntur assumenda. Eum quas fuga ad.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ducimus facere hic ipsa nulla eos tempore quas sequi neque odio nesciunt quibusdam consequatur nostrum, consequuntur assumenda. Eum quas fuga ad.</p>
+
 </template>
 <script setup lang="ts">
 
